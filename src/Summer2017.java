07be4582fa57bc9a26adfc5638b6dea082dba700
Taras Oleksiv <toleksiv69@gmail.com>
2017-06-25 13:29:15 +0300

    Create project for Module1.

---
+++ src/Summer2017.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Taras on 25.06.2017.
+ */
+public class Summer2017 {
+    public static void main(String[] args){
+        System.out.println("Сподіваюсь, літо 2017 буде приємним.");
+    }
+}
